e4f9bdb4ebc3d9b6841faab94103097f73fdb52b
yellowb <122517167@qq.com>
2018-07-09 11:09:11 +0800

    Add Protobuf definition and classes

---
+++ src/main/java/len_field_based_and_protobuf_demo/ProtobufTest.java
@@ -0,0 +1,20 @@
+package len_field_based_and_protobuf_demo;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+public class ProtobufTest {
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        ExpressionProto.Expression exp = ExpressionProto.Expression.newBuilder().setArg1(1).setArg2(2).setOp("+").build();
+
+        ExpressionProto.Expression exp2 = ExpressionProto.Expression.parseFrom(exp.toByteArray());
+
+        System.out.println(exp2);
+
+        ResultProto.Result rs = ResultProto.Result.newBuilder().setResult(3).build();
+
+        ResultProto.Result rs2 = ResultProto.Result.parseFrom(rs.toByteArray());
+
+        System.out.println(rs2);
+
+    }
+}
