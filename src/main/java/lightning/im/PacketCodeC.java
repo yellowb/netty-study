95657ef235e601b29869aeb58b14459e430a4a42
yellowb <122517167@qq.com>
2018-10-23 10:42:51 +0800

    edit

--- src/main/java/lightning/im/PacketCodeC.java
+++ src/main/java/lightning/im/PacketCodeC.java
@@ -3,9 +3,21 @@ package lightning.im;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 
+import java.util.*;
+
 public class PacketCodeC {
 
     private static final int MAGIC_NUMBER = 0x12345678;
+
+    private static final Map<Byte, Class<? extends Packet>> CLASS_MAP = new HashMap<>();
+
+    private static final Map<Byte, Serializer> SERIALIZER_MAP = new HashMap<>();
+
+    static {
+        CLASS_MAP.put((byte)1, LoginRequestPacket.class);
+
+        SERIALIZER_MAP.put(SerializerAlgorithm.JSON, new JSONSerializer());
+    }
 
     public ByteBuf encode(Packet packet) {
         ByteBuf byteBuf = ByteBufAllocator.DEFAULT.ioBuffer();
@@ -21,6 +33,44 @@ public class PacketCodeC {
         byteBuf.writeBytes(bytes);
 
         return byteBuf;
+    }
+
+    public Packet decode(ByteBuf byteBuf) {
+        // skip magic number
+        byteBuf.skipBytes(4);
+
+        // skip version number
+        byteBuf.skipBytes(1);
+
+        // 序列化算法标识
+        byte serializeAlgorithm = byteBuf.readByte();
+
+        // command
+        byte command = byteBuf.readByte();
+
+        // body length
+        int length = byteBuf.readInt();
+
+        // body
+        byte[] bytes = new byte[length];
+        byteBuf.readBytes(bytes);
+
+        Class<? extends Packet> requestType = getRequestType(command);
+        Serializer serializer = getSerializer(serializeAlgorithm);
+
+        if(requestType != null && serializer != null) {
+            return serializer.deserialize(requestType, bytes);
+        }
+
+        return null;
+    }
+
+    private Class<? extends Packet> getRequestType(byte classType) {
+        return CLASS_MAP.get(classType);
+    }
+
+    private Serializer getSerializer(byte serializeAlgorithm) {
+        return SERIALIZER_MAP.get(serializeAlgorithm);
     }
 
 }
