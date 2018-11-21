ae648774965484721dfd0137e4fc3626c15ef705
yellowb <122517167@qq.com>
2018-11-21 11:01:22 +0800

    edit

--- src/main/java/lightning/im2/HeartBeatRequestHandler.java
+++ src/main/java/lightning/im2/HeartBeatRequestHandler.java
@@ -5,6 +5,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import lightning.im.HeartBeatRequestPacket;
 import lightning.im.HeartBeatResponsePacket;
+import lightning.im.LoginUtil;
 
 import java.util.Date;
 
@@ -15,7 +16,7 @@ public class HeartBeatRequestHandler extends SimpleChannelInboundHandler<HeartBe
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HeartBeatRequestPacket msg) throws Exception {
-        System.out.println(new Date() + ": 收到客户端心跳包");
+        System.out.println(new Date() + ": 收到客户端[" + ctx.channel().remoteAddress() +"]心跳包");
         ctx.writeAndFlush(new HeartBeatResponsePacket());
     }
 }
